751189d39224a87f7efab3e16d77eb596f36afff
Felipe Cannarozzo <felipe.lourenco.ca@gmail.com>
2023-07-11 01:31:51 -0300

    Exemplo de tipos de dados básicos em Java

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+	
+	public static void main(String [] args) {
+		
+		int inteiro = 10;
+		float reais = 10.5f;
+		boolean aberto = false;
+		char letra = 'A';
+		String palavra = "Varios caracteres";
+		
+		System.out.println("Valor do tipo inteiro: " + inteiro);
+		System.out.println("Valor do tipo real/float: " + reais);
+		System.out.println("Valor do tipo logico/boolean: " + aberto);
+		System.out.println("Valor do tipo caracter: " + letra);
+		System.out.println("Valor do tipo String/cadeia: " + palavra);
+		
+		
+	}
+
+}
